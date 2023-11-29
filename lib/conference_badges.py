74da1179592175d5338dd2b0c507f8fa43e7bef9
Morinke01 <ejulius250@gmail.com>
2023-11-29 08:45:56 +0300

    Assignment completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    return [f'Hello, my name is {name}.' for name in names]
 
 def assign_rooms(names):
-    return None
+    if len(names) > 8:
+        raise ValueError("Not enough rooms for all speakers.")
+
+    room_assignments = []
+
+    for room, speaker in zip(range(1, 9), names):
+        room_assignments.append(f"Hello, {speaker}! You'll be assigned to room {room}!")
+
+    return room_assignments
 
 def printer(names):
-    return None
+    badge_messages = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    for badge_message in badge_messages:
+        print(badge_message)
+    
+    for room_assignment in room_assignments:
+        print(room_assignment)
